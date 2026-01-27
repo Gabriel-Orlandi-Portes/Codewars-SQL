d9a905559a109b22870d70df365b1b0c655b636e
Gabriel Orlandi Portes <163611471+Gabriel-Orlandi-Portes@users.noreply.github.com>
2026-01-27 16:45:16 -0300

    SQL Basics: Simple DISTINCT

---
+++ Codewars/8Kyu/SQL Basics Simple DISTINCT.sql
@@ -0,0 +1,18 @@
+-- Codewars SQL
+-- Nível: 8 kyu
+-- Desafio: Consulta com DISTINCT
+
+-- Contexto:
+-- encontrar idades únicas.
+
+-- Objetivo:
+-- Remover duplicatas.
+
+-- Tabela:
+-- people
+-- - id (integer)
+-- - name (string)
+-- - age (date)
+
+SELECT DISTINCT age 
+FROM people
